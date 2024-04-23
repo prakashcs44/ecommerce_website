4b8982e9926dbe935b28b2b7c86a7c2734e432ee
prakashcs44 <porkashbatarai@gmail.com>
2024-04-23 20:44:59 +0530

    added configs for dev

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(fileUpload())
 
 app.use(cors({
-    origin:process.env.CLIENT_ORIGIN||"*",
+    origin:process.env.CLIENT_ORIGIN||"http://localhost:5173",
     credentials:true,
     
 }));
