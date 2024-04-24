81ba8828b0841ec68bf659ae9998c0ad5252df5d
prakashcs44 <porkashbatarai@gmail.com>
2024-04-24 13:54:24 +0530

    added maximum request size for json

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const fileUpload = require("express-fileupload");
 
 
 
-app.use(express.json());
+app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(fileUpload())
