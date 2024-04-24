2e3b28421261716b27257fe8a2556ad182d11b76
prakashcs44 <porkashbatarai@gmail.com>
2024-04-24 11:06:54 +0530

    added console log to show error

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -29,7 +29,7 @@ module.exports = (err,req,res,next)=>{
   err.statusCode = err.statusCode||500;
  
   err.message = err.message||"Internal server error";
- 
+   console.log(err.message);
     res.status(err.statusCode).json({
        success:false,
        message:err.message
