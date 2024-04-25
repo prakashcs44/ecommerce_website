9ce6cca146f7a0c12ed240034b0fc52cf1f4212a
prakashcs44 <porkashbatarai@gmail.com>
2024-04-25 17:07:22 +0530

    button chnage

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+import onDev from "../dev.js"
 
 const axiosClient = axios.create({
-    baseURL: `https://ecommerce-website-lm3b.onrender.com/api`,
+    baseURL: onDev?"http://localhost:4000/api":`https://ecommerce-website-lm3b.onrender.com/api`,
     headers:{
       "Content-Type":"application/json",
     },
