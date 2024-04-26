9e63f6d97f6fbafc6d2a3e0965291887a03cd1df
prakashcs44 <porkashbatarai@gmail.com>
2024-04-26 11:26:33 +0530

    changed ui

--- client/src/pages/user/Cart.jsx
+++ client/src/pages/user/Cart.jsx
@@ -1,69 +1,74 @@
-import React, { useEffect } from 'react';
-import { useSelector,useDispatch} from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
-import Typography from '@mui/material/Typography';
 import CartItem from "../../components/CartItem";
-import MetaData from '../../components/layout/MetaData';
-import { Button } from '@mui/material';
-import { getCartItems } from '../../redux/slices/cartSlice';
-
+import MetaData from "../../components/layout/MetaData";
+import { Button } from "@mui/material";
+import { getCartItems } from "../../redux/slices/cartSlice";
+import NothingToShow from "../../components/NothingToShow";
 
 function Cart() {
-  const { cartItems } = useSelector(store => store.cart);
-   const dispatch = useDispatch();
-  
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(getCartItems())
-  },[])
-  
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
 
-  
- 
+  if (cartItems && cartItems.length == 0) {
+    return (
+      <NothingToShow
+        title=" No Products in Your Cart"
+        redirectTitle="View Products"
+        redirect="/products"
+      />
+    );
+  }
+
   return (
     <>
-    <MetaData title="My Cart"/>
-    <div className="container mx-auto px-10">
-      {cartItems.length === 0 ? (
-        <div className="flex flex-col items-center justify-center h-screen">
-          <RemoveShoppingCartIcon className="text-4xl text-gray-500 mb-4" />
-          <Typography variant="h5" className="text-gray-500 mb-4">
-            No Products in Your Cart
-          </Typography>
-          <Link to="/products" className="text-blue-500">
-            View Products
-          </Link>
+      <MetaData title="My Cart" />
+
+      <div className="flex-1  px-4 py-6 sm:px-6">
+        <div className="flex items-start justify-center border-b-2 py-3">
+          <h1 className="text-2xl font-medium text-gray-900 ">Shopping cart</h1>
         </div>
-      ) : (
+
         <div className="mt-8">
-          <div className="flex justify-between items-center mb-4">
-            <p className=" font-bold">Product</p>
-            <p className=" font-bold flex justify-center">Quantity</p>
-            <p className=" font-bold flex justify-end">Subtotal</p>
-          </div>
-
-          {cartItems.map((item) => (
-            <CartItem item = {item} key={item.product}/>
-          ))}
-
-          <div className="flex justify-end items-center mt-8">
-            <div className="w-1/3"></div>
-            <div className="w-1/3">
-              <p className="font-bold text-center">Gross Total</p>
-              <p className="text-center">{`₹${cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0)}`}</p>
-            </div>
-            <div className="w-1/3 flex justify-end">
-              <Button 
-               variant='contained'
-              >
-                <Link to="/shipping">Check out</Link>
-              </Button>
-            </div>
+          <div className="-my-6 divide-y divide-gray-200">
+            {cartItems.map((item) => (
+              <CartItem item={item} key={item.product} />
+            ))}
           </div>
         </div>
-      )}
-    </div>
+      </div>
+
+      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+        <div className="flex justify-between text-base font-medium text-gray-900">
+          <p>Subtotal</p>
+          <p>{`₹${cartItems.reduce(
+            (acc, item) => acc + item.quantity * item.price,
+            0
+          )}`}</p>
+        </div>
+
+        <div className="text-center ">
+          <Button variant="contained">
+            <Link to="/shipping">Checkout</Link>
+          </Button>
+        </div>
+
+        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+          <Button
+            type="button"
+            className="font-medium text-indigo-600 hover:text-indigo-500"
+          >
+            <Link to="/products">Continue Shopping</Link>
+
+            <span aria-hidden="true"> &rarr;</span>
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
