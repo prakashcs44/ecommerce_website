9d653c72f45204cbb909a7112f58a0d29ae60d5d
prakashcs44 <porkashbatarai@gmail.com>
2024-05-01 17:57:23 +0530

    added user img

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Rating from '../components/product/Rating';
+import userImg from "../assets/default_user.jpg"
 
 function ReviewCard({ review }) {
+  console.log(review);
   
   return (
     <div className="bg-white rounded-lg shadow-md p-4 mb-4">
-      <img className="w-16 h-16 rounded-full object-cover mb-2" src="https://source.unsplash.com/random" alt="User Avatar" />
+      <img className="w-16 h-16 rounded-full object-cover mb-2" src={userImg} alt="User Avatar" />
       <p className="font-semibold mb-1">{review?.name}</p>
       <div className="flex flex-col items-center mb-1">
         <Rating  readOnly = {true} value = {review?.rating}/>
