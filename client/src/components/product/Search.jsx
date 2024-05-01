0b1ac2d313c6c01d13a0d2f23cf1395be46613ee
prakashcs44 <porkashbatarai@gmail.com>
2024-05-01 19:50:45 +0530

    updated

--- client/src/components/product/Search.jsx
+++ client/src/components/product/Search.jsx
@@ -1,24 +1,18 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
 
-function Search() {
-    const [keyword, setKeyword] = useState("");
-    const navigate = useNavigate();
 
-    const searchSubmitHandler = (ev) => {
-        ev.preventDefault();
-        if(keyword.trim()){
-           navigate(`/products/${keyword}`);
-        }
-    };
+function Search({value,onChange,onSearch}) {
+
+    
 
     return (
-        <form onSubmit={searchSubmitHandler} className="flex flex-col sm:flex-row">
+        <form onSubmit={onSearch} className="flex flex-col sm:flex-row">
             <input
                 type='text'
                 placeholder='Search a product...'
                 className="border rounded-l px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2 sm:mb-0 sm:mr-2"
-                onChange={(ev)=>setKeyword(ev.target.value)}
+                onChange={onChange}
+                value={value}
             />
             <button
                 type='submit'
