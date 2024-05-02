8c29f3461660a8a751b5c7d920f2adeef08f0a07
prakashcs44 <porkashbatarai@gmail.com>
2024-05-02 13:41:13 +0530

    added image login

--- client/src/pages/user/Auth.jsx
+++ client/src/pages/user/Auth.jsx
@@ -11,7 +11,7 @@ import {
   REGISTER_USER_REQUEST,
 } from "../../redux/constants/user.js";
 import SensitiveInput from "../../components/SensitiveInput.jsx";
-
+import authImg from "../../assets/login.jpg"
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -71,8 +71,9 @@ const Auth = () => {
   };
 
   return (
-    <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div className=" flex flex-col   lg:flex-row items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 lg:mr-40">
+      <img src={authImg} className="size-[90vmin] md:size-[70vmin]"/>
+      <div className="space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             {isLogin ? "Sign in to your account" : "Register an account"}
