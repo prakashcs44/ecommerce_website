b40e1c47f7ab78734366656e1bb7c5f827225407
prakashcs44 <porkashbatarai@gmail.com>
2024-05-05 08:42:53 +0530

    added server uri in secure way

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -1,14 +1,19 @@
 import axios from "axios";
+const serverUri= import.meta.env.VITE_SERVER_URI;
 
 
-const axiosClient = axios.create({
-    baseURL: `https://ecommerce-website-lm3b.onrender.com/api`,
+
+
+
+  const axiosClient = axios.create({
+    baseURL: serverUri,
     headers:{
       "Content-Type":"application/json",
     },
     withCredentials:true,
    
   });
+
 
 
 export default axiosClient;
