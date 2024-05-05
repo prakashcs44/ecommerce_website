b9bbfe0ca6a1bb79600d8894507ee3d030d3d417
prakashcs44 <porkashbatarai@gmail.com>
2024-05-05 11:41:21 +0530

    changed input type to search

--- client/src/components/product/Search.jsx
+++ client/src/components/product/Search.jsx
@@ -8,7 +8,7 @@ function Search({value,onChange,onSearch}) {
     return (
         <form onSubmit={onSearch} className="flex flex-col sm:flex-row">
             <input
-                type='text'
+                type='search'
                 placeholder='Search a product...'
                 className="border rounded-l px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2 sm:mb-0 sm:mr-2"
                 onChange={onChange}
