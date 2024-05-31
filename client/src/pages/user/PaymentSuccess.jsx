723224da6c736984182d4afd8560e19cd4772532
prakashcs44 <porkashbatarai@gmail.com>
2024-05-31 19:15:19 +0530

    added metadata

--- client/src/pages/user/PaymentSuccess.jsx
+++ client/src/pages/user/PaymentSuccess.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import MetaData from "../../components/layout/MetaData";
 
 const PaymentSuccess = () => {
   return (
+    <>
+    <MetaData title = "PAYMENT SUCCESS"/>
     <div className="flex flex-col items-center justify-center h-screen gap-2">
       <CheckCircleIcon  />
       <Typography variant="h5">Your Order has been Placed successfully </Typography>
@@ -13,6 +16,7 @@ const PaymentSuccess = () => {
       </Button>
       
     </div>
+    </>
   );
 };
 
